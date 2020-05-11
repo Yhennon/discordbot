5c2efc6d4ad10b84bb44a48f39822ed43bb75d8f
Yhennon <loostomi@windowslive.com>
2020-05-11 11:44:05 +0200

    Added bot

--- orabot.py
+++ orabot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-with open("orabot_food.txt") as f:
+with open("../orabot_food.txt") as f:
 	auth = f.readlines()
 auth = [x.strip() for x in auth]
 
@@ -11,5 +11,6 @@ CHANNEL_ID = int(auth[1])
 @bot.command()
 async def szia(ctx):
 	await ctx.send("Szia")
+	
 
 bot.run(auth[0])
